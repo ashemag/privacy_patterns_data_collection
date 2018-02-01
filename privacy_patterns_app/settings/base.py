9404d05f9baeee29d5beaf1b5b42eb85d53b3739
ashemag <ashemag@stanford.edu>
2018-02-02 00:44:54 +0100

    ugh

--- privacy_patterns_app/settings/base.py
+++ privacy_patterns_app/settings/base.py
@@ -11,15 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Static files (CSS, JavaScript, Images)
