c299543bb13394e41037ee27e96121d65d65ea1a
ashemag <ashemag@stanford.edu>
2018-02-02 00:42:24 +0100

    ugh

--- privacy_patterns_app/settings/__init__.py
+++ privacy_patterns_app/settings/__init__.py
@@ -2,4 +2,11 @@
 # ~/Dev/cfehome/src/cfehome/settings/ on mac/linux
 # \Users\YourName\Dev\cfehome\src\cfehome\settings\ on windows
 
-echo 'from .base import * from .production import * try: from .local import * except: pass' > __init__.py
+from .base import *
+
+from .production import *
+
+try:
+	from .local import *
+except:
+	pass
