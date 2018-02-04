8a07d57df30865af1954124740a8f0cc7d4fe80a
ashemag <ashemag@stanford.edu>
2018-02-04 22:49:27 +0100

    trying diff url config

--- privacy_patterns_app/wsgi.py
+++ privacy_patterns_app/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "privacy_patterns_app.settings")
 
 application = get_wsgi_application()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "privacy_patterns_app.settings")
-application = WhiteNoise(application, root='../display_data_app/static')
-# application.add_files('/path/to/more/static/files', prefix='more-files/')
